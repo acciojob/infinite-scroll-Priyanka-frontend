a7c0467f7195fd90cf36210ad7a2ec262a1ab2eb
Priyanka-frontend <kaushikpriyanka005@gmail.com>
2026-06-13 14:38:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-//your code here!
+const list = document.getElementById("infi-list");
+let count = 0;
 
+// Function to add list items
+function addItems(num) {
+  for (let i = 0; i < num; i++) {
+    count++;
+
+    const li = document.createElement("li");
+    li.textContent = `Item ${count}`;
+
+    list.appendChild(li);
+  }
+}
+
+// Add 10 items initially
+addItems(10);
+
+// Infinite scrolling
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >=
+    document.body.offsetHeight
+  ) {
+    addItems(2);
+  }
+});
